9aa4106f0a9773760fc1d44b318c0ea89639bf0f
Mapleaves <15229255171@163.com>
2019-11-06 16:02:39 +0800

    Update routes.js
    添加个人设置和备件管理路径

--- src/routes.js
+++ src/routes.js
@@ -3,16 +3,19 @@ import UserLogin from './user/login.vue';
 import Layout from './layout/layout.vue';
 import Content from './layout/content/content.vue';
 import PageOne from './page/test/pageone.vue';
+import UserSetting from './page/account/setting/index.vue';
+import eamPartLibrary from './page/eam/eamPartLibrary/index.vue';
 const routes = [
     { path: '/',name:'home',component:Layout,
     children: [
         {path: '', name:"portal", component: Portal },
         {path: 'page', name:"content", component: Content,children: [
             {path: 'test/pageone', name:"pageone", component: PageOne },
-        ]}
+            {path: 'test/pageone', name:"pageone", component: PageOne },
+            {path: 'account/setting/index', name:'index',component: UserSetting },
+            {path: 'eam/eamPartLibrary/index', name:'index',component: eamPartLibrary }
+        ]},
       ] },
     { path: '/user/login', name:'userLogin',component: UserLogin }
 ];
- 
- 
 export default routes;
