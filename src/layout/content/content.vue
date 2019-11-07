a4eb7f11891fec032a98a3c5c91f6f0f7c3b34e8
Mapleaves <15229255171@163.com>
2019-11-07 18:48:35 +0800

    Update content.vue
    修改样式

--- src/layout/content/content.vue
+++ src/layout/content/content.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
-    <el-breadcrumb separator-class="el-icon-arrow-right">
-      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-      <el-breadcrumb-item>活动管理</el-breadcrumb-item>
-      <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-      <el-breadcrumb-item>活动详情</el-breadcrumb-item>
-    </el-breadcrumb>
+      <el-breadcrumb separator-class="el-icon-arrow-right" style="margin:10px;">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+        <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+        <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+      </el-breadcrumb>
+    <br>
     <router-view></router-view>
   </div>
 </template>
