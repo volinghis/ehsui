9cdc1543e21ae6c951b0d70fe205629aea4e61e0
Mapleaves <15229255171@163.com>
2019-11-08 19:59:53 +0800

    Update index.vue
    备件列表

---
+++ src/page/eam/eamPartLibrary/index.vue
@@ -0,0 +1,191 @@
+<template>
+    <el-card :bordered="false">
+        <div class="table-page-search-wrapper">
+        <el-form :inline="true" :model="queryParam" class="demo-form-inline" size="small">
+            <el-row :gutter="48">
+            <el-col :md="8" :sm="24">
+                <el-form-item label="备件编号：">
+                    <el-input v-model="queryParam.code" placeholder="请输入备件编号" style="width: 300px;"/>
+                </el-form-item>
+            </el-col>
+            <el-col :md="8" :sm="24">
+                <el-form-item label="备件名称：">
+                    <el-input v-model="queryParam.name" placeholder="请输入备件名称" style="width: 300px;"/>
+                </el-form-item>
+            </el-col>
+            <template v-if="advanced">
+                <el-col :md="8" :sm="24">
+                    <el-form-item label="调用次数：">
+                        <el-input  placeholder="" style="width: 300px;"/>
+                    </el-form-item>
+                </el-col>
+                <el-col :md="8" :sm="24">
+                    <el-form-item label="更新日期：">
+                        <el-input  placeholder="" style="width: 300px;"/>
+                    </el-form-item>
+                </el-col>
+                <el-col :md="8" :sm="24">
+                    <el-form-item label="使用状态：">
+                        <el-input  placeholder="" style="width: 300px;"/>
+                    </el-form-item>
+                </el-col>
+                <el-col :md="8" :sm="24">
+                    <el-form-item label="使用状态：">
+                        <el-input  placeholder="" style="width: 300px;"/>
+                    </el-form-item>
+                </el-col>
+            </template>
+            <el-col :md="!advanced && 8 || 24" :sm="24">
+                <span class="table-page-search-submitButtons" :style="advanced && { float: 'right', overflow: 'hidden' } || {} " >
+                    <el-button type="primary" @click="$refs.table.refresh(true)" size="small">查询</el-button>
+                    <el-button style="margin-left: 8px" @click="() => queryParam = {}" size="small">重置</el-button>
+                    <a @click="toggleAdvanced" style="margin-left: 8px" >
+                        <span style="font-size:small;color:#409EFF">{{ advanced ? '收起' : '展开' }}
+                            <i :class="advanced ? 'el-icon-arrow-up' : 'el-icon-arrow-down'"/>
+                        </span>
+                    </a>
+                </span>
+            </el-col>
+            </el-row>
+        </el-form>
+        </div>
+        <div class="table-operator">
+            <el-button type="primary" icon="el-icon-plus" style="float:left;">新增</el-button>
+        </div>
+        <template>
+            <el-table :data="tableData" border style="width: 100%">
+                <el-table-column fixed type="index" width="50"> </el-table-column>
+                <el-table-column fixed prop="code" label="备件编号" width="150"> </el-table-column>
+                <el-table-column fixed prop="name" label="备件名称" width="120"> </el-table-column>
+                <el-table-column fixed prop="completion" label="资料完整度" width="200">
+                    <template slot-scope="scope">
+                        <el-progress :percentage="scope.row.completion" :color="customColors" ></el-progress>
+                    </template>
+                </el-table-column>
+                <el-table-column prop="type" label="备件类型" width="120"> </el-table-column>
+                <el-table-column prop="model" label="备件型号" width="120"> </el-table-column>
+                <el-table-column prop="norm" label="备件规格" width="120"> </el-table-column>
+                <el-table-column prop="brand" label="品牌" width="120"> </el-table-column>
+                <el-table-column prop="warehouse" label="所在仓库" width="120"> </el-table-column>
+                <el-table-column prop="unit" label="单位" width="120"> </el-table-column>
+                <el-table-column prop="price" label="价格" width="120"> </el-table-column>
+                <el-table-column prop="amount" label="数量" width="120"> </el-table-column>
+                <el-table-column prop="warningValue" label="库存预警值" width="120"> </el-table-column>
+                <el-table-column fixed="right" label="操作" width="150">
+                <template slot-scope="scope">
+                    <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+                    <el-button type="text" size="small">编辑</el-button>
+                </template>
+                </el-table-column>
+            </el-table>
+            </template>
+    </el-card>
+</template>
+
+<script>
+  export default {
+    methods: {
+        handleClick(row) {
+            console.log(row);
+        },
+        toggleAdvanced () {
+            this.advanced = !this.advanced
+        }
+    },
+    data() {
+      return {
+          // 高级搜索 展开/关闭
+        advanced: false,
+        // 查询参数
+        queryParam: {},
+        customColor: '#409eff',
+        customColors: [
+            {color: '#f56c6c', percentage: 20},
+            {color: '#e6a23c', percentage: 40},
+            {color: '#5cb87a', percentage: 60},
+            {color: '#1989fa', percentage: 80},
+            {color: '#6f7ad3', percentage: 100}
+        ],
+        tableData: [{
+          code: 'FM-0001',
+          name: '阀门',
+          completion:80,
+          type: '阀门',
+          model: '阀门',
+          norm: 'FM_0001_01',
+          brand: '上海市普陀牌',
+          warehouse: '1号仓库',
+          unit: '个',
+          price: '100',
+          amount: '56',
+          warningValue: '10'
+        },{
+          code: 'FM-0001',
+          name: '阀门',
+          completion:80,
+          type: '阀门',
+          model: '阀门',
+          norm: 'FM_0001_01',
+          brand: '上海市普陀牌',
+          warehouse: '1号仓库',
+          unit: '个',
+          price: '100',
+          amount: '56',
+          warningValue: '10'
+        },{
+          code: 'FM-0001',
+          name: '阀门',
+          completion:80,
+          type: '阀门',
+          model: '阀门',
+          norm: 'FM_0001_01',
+          brand: '上海市普陀牌',
+          warehouse: '1号仓库',
+          unit: '个',
+          price: '100',
+          amount: '56',
+          warningValue: '10'
+        },{
+          code: 'FM-0001',
+          name: '阀门',
+          completion:80,
+          type: '阀门',
+          model: '阀门',
+          norm: 'FM_0001_01',
+          brand: '上海市普陀牌',
+          warehouse: '1号仓库',
+          unit: '个',
+          price: '100',
+          amount: '56',
+          warningValue: '10'
+        },{
+          code: 'FM-0001',
+          name: '阀门',
+          completion:80,
+          type: '阀门',
+          model: '阀门',
+          norm: 'FM_0001_01',
+          brand: '上海市普陀牌',
+          warehouse: '1号仓库',
+          unit: '个',
+          price: '100',
+          amount: '56',
+          warningValue: '10'
+        },{
+          code: 'FM-0001',
+          name: '阀门',
+          completion:80,
+          type: '阀门',
+          model: '阀门',
+          norm: 'FM_0001_01',
+          brand: '上海市普陀牌',
+          warehouse: '1号仓库',
+          unit: '个',
+          price: '100',
+          amount: '56',
+          warningValue: '10'
+        }]
+      }
+    }
+  }
+</script>
