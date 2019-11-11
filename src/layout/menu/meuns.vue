b5c9c7e973d3df9a7f6f8d19c9e5dfd756bc0c16
selfvolinghis <2629106344@qq.com>
2019-11-11 09:28:34 +0800

    格式化代码

--- src/layout/menu/meuns.vue
+++ src/layout/menu/meuns.vue
@@ -44,7 +44,6 @@
   </el-menu>
 </template>
 
-
 <style scoped>
 .el-menu {
   float: left;
@@ -52,69 +51,71 @@
 </style>
 <script scoped>
 export default {
-  data() {
+  data () {
     return {
       menutotalwidth: 0,
       showMenus: [],
       hideMenus: [],
       menus: [
-        { index: "/", key: "1", label: "首页" },
+        { index: '/', key: '1', label: '首页' },
         {
-          index: "/page/test/pageone",
-          key: "2",
-          label: "设备管理",
+          index: '/page/test/pageone',
+          key: '2',
+          label: '设备管理',
           children: [
-            { index: "/page/eam/eamAccountPrint/index", key: "eamAccountPrint", label: "设备列表" },
-            { index: "/page/eam/eamAccountPrint/details", key: "223", label: "设备详情" }
+            { index: '/page/eam/eamAccountPrint/index', key: 'eamAccountPrint', label: '设备列表' },
+            { index: '/page/eam/eamAccountPrint/details', key: '223', label: '设备详情' }
           ]
         },
         {
-          index: "/",
-          key: "3",
-          label: "备件管理",
+          index: '/',
+          key: '3',
+          label: '备件管理',
           children: [
-            { index: "/page/eam/eamPartLibrary/details", key: "details", label: "备件详情" },
-            { index: "/page/eam/eamPartLibrary/index", key: "index", label: "备件台账" }
+            { index: '/page/eam/eamPartLibrary/details', key: 'details', label: '备件详情' },
+            { index: '/page/eam/eamPartLibrary/index', key: 'index', label: '备件台账' }
           ]
         },
-        { index: "/page/test/pageone", key: "4", label: "测试4" },
-        { index: "/page/test/pageone", key: "5", label: "测试5" },
-        { index: "/page/test/pageone", key: "6", label: "测试6" },
-        { index: "/page/test/pageone3", key: "7", label: "测试7" },
-        { index: "/page/test/pageone4", key: "8", label: "测试8" },
-        { index: "/page/test/pageone5", key: "9", label: "测试9" },
-        { index: "/page/test/pageone6", key: "10", label: "测试10" },
+        { index: '/page/test/pageone', key: '4', label: '测试4' },
+        { index: '/page/test/pageone', key: '5', label: '测试5' },
+        { index: '/page/test/pageone', key: '6', label: '测试6' },
+        { index: '/page/test/pageone3', key: '7', label: '测试7' },
+        { index: '/page/test/pageone4', key: '8', label: '测试8' },
+        { index: '/page/test/pageone5', key: '9', label: '测试9' },
+        { index: '/page/test/pageone6', key: '10', label: '测试10' },
         {
-          index: "/page/test/pageone12",key: "11", label: "测试113",
+          index: '/page/test/pageone12',
+          key: '11',
+          label: '测试113',
           children: [
-            { index: "/page/test/pageone3", key: "12", label: "测试132" },
-            { index: "/page/test/pageone35", key: "13", label: "测试133" }
+            { index: '/page/test/pageone3', key: '12', label: '测试132' },
+            { index: '/page/test/pageone35', key: '13', label: '测试133' }
           ]
         }
       ]
-    };
+    }
   },
-  mounted: function() {
-    var elwidth = document.querySelector(".el-header").offsetWidth;
+  mounted: function () {
+    var elwidth = document.querySelector('.el-header').offsetWidth
     this.menutotalwidth =
       elwidth -
       elwidth * 0.1 * 2 -
-      document.querySelector("#logopanel").offsetWidth -
-      document.querySelector("#userpanel").offsetWidth;
-    this.menutotalwidth = this.menutotalwidth < 200 ? 200 : this.menutotalwidth;
+      document.querySelector('#logopanel').offsetWidth -
+      document.querySelector('#userpanel').offsetWidth
+    this.menutotalwidth = this.menutotalwidth < 200 ? 200 : this.menutotalwidth
     for (var i = 0; i < this.menus.length; i++) {
       if (i < parseInt(this.menutotalwidth / 100) - 1) {
-        this.showMenus.push(this.menus[i]);
-      } else if (i == parseInt(this.menutotalwidth / 100) - 1) {
+        this.showMenus.push(this.menus[i])
+      } else if (i === parseInt(this.menutotalwidth / 100) - 1) {
         this.hideMenus.push({
-          index: "",
-          label: "更多",
+          index: '',
+          label: '更多',
           children: [this.menus[i]]
-        });
+        })
       } else {
-        this.hideMenus[0].children.push(this.menus[i]);
+        this.hideMenus[0].children.push(this.menus[i])
       }
     }
   }
-};
+}
 </script>
--- src/layout/user/user.vue
+++ src/layout/user/user.vue
@@ -26,8 +26,6 @@
           <i class="el-icon-view" size="small"></i>
     </el-tooltip>
 
-   
-
   <el-autocomplete
   class="query"
   size="small"
@@ -41,49 +39,49 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        restaurants: [],
-        state: '',
-        timeout:  null
-      };
+export default {
+  data () {
+    return {
+      restaurants: [],
+      state: '',
+      timeout: null
+    }
+  },
+  methods: {
+    loadAll () {
+      return [
+        { 'value': '三全鲜食（北新泾店）', 'address': '长宁区新渔路144号' },
+        { 'value': 'Hot honey 首尔炸鸡（仙霞路）', 'address': '上海市长宁区淞虹路661号' },
+        { 'value': '新旺角茶餐厅', 'address': '上海市普陀区真北路988号创邑金沙谷6号楼113' },
+        { 'value': '泷千家(天山西路店)', 'address': '天山西路438号' },
+        { 'value': '胖仙女纸杯蛋糕（上海凌空店）', 'address': '上海市长宁区金钟路968号1幢18号楼一层商铺18-101' },
+        { 'value': '贡茶', 'address': '上海市长宁区金钟路633号' },
+        { 'value': '豪大大香鸡排超级奶爸', 'address': '上海市嘉定区曹安公路曹安路1685号' },
+        { 'value': '茶芝兰（奶茶，手抓饼）', 'address': '上海市普陀区同普路1435号' }
+      ]
     },
-    methods: {
-      loadAll() {
-        return [
-          { "value": "三全鲜食（北新泾店）", "address": "长宁区新渔路144号" },
-          { "value": "Hot honey 首尔炸鸡（仙霞路）", "address": "上海市长宁区淞虹路661号" },
-          { "value": "新旺角茶餐厅", "address": "上海市普陀区真北路988号创邑金沙谷6号楼113" },
-          { "value": "泷千家(天山西路店)", "address": "天山西路438号" },
-          { "value": "胖仙女纸杯蛋糕（上海凌空店）", "address": "上海市长宁区金钟路968号1幢18号楼一层商铺18-101" },
-          { "value": "贡茶", "address": "上海市长宁区金钟路633号" },
-          { "value": "豪大大香鸡排超级奶爸", "address": "上海市嘉定区曹安公路曹安路1685号" },
-          { "value": "茶芝兰（奶茶，手抓饼）", "address": "上海市普陀区同普路1435号" }
-        ];
-      },
-      querySearchAsync(queryString, cb) {
-        var restaurants = this.restaurants;
-        var results = queryString ? restaurants.filter(this.createStateFilter(queryString)) : restaurants;
+    querySearchAsync (queryString, cb) {
+      var restaurants = this.restaurants
+      var results = queryString ? restaurants.filter(this.createStateFilter(queryString)) : restaurants
 
-        clearTimeout(this.timeout);
-        this.timeout = setTimeout(() => {
-          cb(results);
-        }, 3000 * Math.random());
-      },
-      createStateFilter(queryString) {
-        return (state) => {
-          return (state.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
-        };
-      },
-      handleSelect(item) {
-        console.log(item);
+      clearTimeout(this.timeout)
+      this.timeout = setTimeout(() => {
+        cb(results)
+      }, 3000 * Math.random())
+    },
+    createStateFilter (queryString) {
+      return (state) => {
+        return (state.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0)
       }
     },
-    mounted() {
-      this.restaurants = this.loadAll();
+    handleSelect (item) {
+      console.log(item)
     }
-  };
+  },
+  mounted () {
+    this.restaurants = this.loadAll()
+  }
+}
 </script>
 <style scoped>
 #userpanel{
