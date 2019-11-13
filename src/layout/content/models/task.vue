402dbd5373211cda9c53e9bb14ea8a29722d8111
qjj <584419234@qq.com>
2019-11-13 17:16:40 +0800

    首页
    首页布局更改

---
+++ src/layout/content/models/task.vue
@@ -0,0 +1,59 @@
+<template>
+  <el-tabs v-model="activeName"
+           @tab-click="handleClick">
+    <el-tab-pane label="流程"
+                 name="first">
+      <el-table :data="tableData"
+                size="mini">
+        <el-table-column type="index"
+                         :index="indexMethod">
+        </el-table-column>
+        <el-table-column prop="date"
+                         label="日期"> </el-table-column>
+        <el-table-column prop="name"
+                         label="流转"> </el-table-column>
+        <el-table-column prop="status" label="状态"> </el-table-column>
+      </el-table>
+    </el-tab-pane>
+    <el-tab-pane label="任务"
+                 name="second">任务管理</el-tab-pane>
+    <el-tab-pane label="计划"
+                 name="third">计划管理</el-tab-pane>
+  </el-tabs>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      activeName: 'first',
+      tableData: [
+        {
+          date: '2016/05/02',
+          name: '王小虎',
+          status: '申请中'
+        },
+        {
+          date: '2016/05/04',
+          name: '王小虎',
+          status: '驳回'
+        },
+        {
+          date: '2016/05/01',
+          name: '王小虎',
+          status: '完结'
+        },
+        {
+          date: '2016/05/03',
+          name: '王小虎',
+          status: '申请中'
+        }
+      ]
+    }
+  },
+  methods: {
+    handleClick (tab, event) {
+      console.log(tab, event)
+    }
+  }
+}
+</script>
--- src/layout/content/portal.vue
+++ src/layout/content/portal.vue
@@ -1,130 +1,101 @@
 <template>
-  <div>
-    <el-row >
-      <el-col :span="8">
-         <el-card shadow="never">
-          <div slot="header"
-               style="text-align:left;">
-            个人中心
-          </div>
+  <div class="container">
+    <el-row :gutter="10">
+      <el-col  :span="6" :md="24" :lg="6">
+        <el-card shadow="hover">
           <template>
             <centers></centers>
           </template>
         </el-card>
       </el-col>
-      <el-col :span="8">
-        <el-row>
-          <el-col :span="24">
-            <div class="grid-content bg-purple"></div>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="24">
-            <div class="grid-content bg-purple"></div>
-          </el-col>
-        </el-row>
+
+      <el-col :span="12" :md="24" :lg="12">
+        <el-card shadow="hover">
+          <el-row>
+            <el-col :span="12">
+            <div>
+              <charts></charts>
+            </div>
+            </el-col>
+            <el-col :span="12">
+              <div>
+              <charts></charts>
+            </div>
+            </el-col>
+          </el-row>
+        </el-card>
+        <el-card shadow="hover">
+        </el-card>
       </el-col>
-      <el-col :span="8">
-        <div class="grid-content bg-purple"></div>
+
+      <el-col :span="6" :md="24" :lg="6">
+        <el-card shadow="hover">
+          <div slot="header"
+               class="item-title"
+               size="small">代办任务
+          </div>
+          <task></task>
+        </el-card>
+        <el-card shadow="hover">
+        </el-card>
       </el-col>
     </el-row>
-    <el-row >
-      <el-col :span="8">
-       <el-card shadow="never">
-          <div slot="header"
-               style="text-align:left;">
-            精品资料
-          </div>
+    <el-row :gutter="10">
+      <el-col  :span="6" :md="24" :lg="6">
+        <el-card shadow="hover">
           <template>
-            <documents></documents>
+            <centers></centers>
           </template>
         </el-card>
       </el-col>
-      <el-col :span="8">
-        <el-card shadow="never">
-          <div slot="header"
-               style="text-align:left;">
-            设备资料完整度
-          </div>
-          <template>
-            <devices></devices>
-          </template>
+
+      <el-col :span="12" :md="24" :lg="12">
+        <el-card shadow="hover">
+          <el-row>
+            <el-col :span="12">
+            <div>
+              <charts></charts>
+            </div>
+            </el-col>
+            <el-col :span="12">
+              <div>
+              <charts></charts>
+            </div>
+            </el-col>
+          </el-row>
+        </el-card>
+        <el-card shadow="hover">
         </el-card>
       </el-col>
-      <el-col :span="8">
-        <el-card shadow="never">
+
+      <el-col :span="6" :md="24" :lg="6">
+        <el-card shadow="hover">
           <div slot="header"
-               class="clearfix"
-               style="text-align:left;">
-            热门搜索
+               class="item-title"
+               size="small">代办任务
           </div>
-          <div>
-            <charts></charts>
-          </div>
+          <task></task>
+        </el-card>
+        <el-card shadow="hover">
         </el-card>
       </el-col>
     </el-row>
   </div>
 </template>
 <script>
-import charts from './models/charts.vue'
-import devices from './models/devices.vue'
-import documents from './models/documents.vue'
 import centers from './models/centers.vue'
+import charts from './models/charts.vue'
+import task from './models/task.vue'
 export default {
   components: {
+    'centers': centers,
     'charts': charts,
-    'devices': devices,
-    'documents': documents,
-    'centers': centers
+    'task': task
   }
 }
 </script>
 <style scoped>
-
-.el-card {
-  height: 500px;
-  border:none;
-}
-.el-row {
-  margin-bottom: 5px;
-  width: 100%;
-}
-.el-row：last-child {
-  margin-bottom: 0;
-}
-.el-col {
-  border-radius: 4px;
-}
-.bg-purple-dark {
-  background: #99a9bf;
-}
-.bg-purple {
-  background: #d3dce6;
-}
-.bg-purple-light {
-  background: #e5e9f2;
-}
-.grid-content {
-  border-radius: 4px;
-  min-height: 36px;
-}
-.row-bg {
-  padding: 10px 0;
-  background-color: #f9fafc;
-}
-.text {
-  font-size: 14px;
-}
-.item {
-  margin-bottom: 18px;
-}
-.clearfix:before,
-.clearfix:after {
-  display: table;
-  content: "";
-}
-.clearfix:after {
-  clear: both;
+.el-card__header{
+  padding:8px;
 }
 </style>
