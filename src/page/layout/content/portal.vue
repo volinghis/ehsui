adfb9a44596e0bceac1ff2ce7c46d448e4b7574f
Mapleaves <15229255171@163.com>
2019-11-15 18:48:36 +0800

    Update portal.vue
    首页

--- src/page/layout/content/portal.vue
+++ src/page/layout/content/portal.vue
@@ -50,43 +50,51 @@
         </el-card>
       </el-col>
     </el-row>
+    <br>
     <el-row :gutter="10">
-      <el-col  :span="6" :md="24" :lg="6">
+      <el-col :span="6" :md="24" :lg="6">
         <el-card shadow="hover">
           <template>
-            <centers></centers>
+            <div slot="header" class="item-title" size="small" style="text-align:center;border-radius: 30px;">
+                 <el-button size="mini" round type="primary" style="weight:100px;">
+                    <span class="mainTitle">
+                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;贡&nbsp;献&nbsp;度&nbsp;季&nbsp;度&nbsp;排&nbsp;名&nbsp;&nbsp;&nbsp;&nbsp;
+                    </span>
+                  </el-button>
+              </div>
+            <ranking></ranking>
           </template>
         </el-card>
       </el-col>
-
       <el-col :span="12" :md="24" :lg="12">
         <el-card shadow="hover">
-          <el-row>
-            <el-col :span="12">
-            <div>
-              <charts></charts>
-            </div>
-            </el-col>
-            <el-col :span="12">
-              <div>
-              <charts></charts>
-            </div>
-            </el-col>
-          </el-row>
+          <div slot="header" class="item-title" size="small">
+            <span class="mainTitle">新增检修记录</span>
+          </div>
+          <maintenanceRecord></maintenanceRecord>
         </el-card>
+        <br>
         <el-card shadow="hover">
+          <div slot="header" class="item-title" size="small">
+            <span class="mainTitle">动&nbsp;&nbsp;&nbsp;&nbsp;态</span>
+          </div>
+          <dynamic></dynamic>
         </el-card>
       </el-col>
-
       <el-col :span="6" :md="24" :lg="6">
         <el-card shadow="hover">
-          <div slot="header"
-               class="item-title"
-               size="small">代办任务
+          <div slot="header" class="item-title" size="small">
+            <span class="mainTitle">资料完整度</span>
           </div>
-          <task></task>
+          <devices></devices>
         </el-card>
+        <br>
         <el-card shadow="hover">
+          <div slot="header" class="item-title" size="small">
+            <span class="mainTitle">热门关注</span>
+            <a href="#"><span style="float:right;color:#409EFF">更多</span></a>
+          </div>
+          <documents></documents>
         </el-card>
       </el-col>
     </el-row>
@@ -99,6 +107,11 @@ import task from './models/task.vue'
 import accountDoughnut from './models/accountDoughnut.vue'
 import partDoughnut from './models/partDoughnut.vue'
 import linechart from './models/linechart.vue'
+import devices from './models/devices.vue'
+import maintenanceRecord from './models/maintenanceRecord.vue'
+import dynamic from './models/dynamic.vue'
+import ranking from './models/ranking.vue'
+import documents from './models/documents.vue'
 export default {
   components: {
     'centers': centers,
@@ -106,7 +119,12 @@ export default {
     'task': task,
     'accountDoughnut': accountDoughnut,
     'partDoughnut': partDoughnut,
-    'linechart': linechart
+    'linechart': linechart,
+    'devices': devices,
+    'maintenanceRecord': maintenanceRecord,
+    'dynamic': dynamic,
+    'documents': documents,
+    'ranking': ranking
   }
 }
 </script>
