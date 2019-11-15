a5ba0227a52509861baca301345cb1e9178dce48
qjj <584419234@qq.com>
2019-11-15 18:36:12 +0800

    Update portal.vue
    首页

--- src/page/layout/content/portal.vue
+++ src/page/layout/content/portal.vue
@@ -13,30 +13,40 @@
         <el-card shadow="hover">
           <el-row>
             <el-col :span="12">
-            <div>
-              <charts></charts>
-            </div>
+              <!-- <div>设备概况</div> -->
+              <accountDoughnut></accountDoughnut>
             </el-col>
             <el-col :span="12">
               <div>
-              <charts></charts>
-            </div>
+                <!-- <div>备件概况</div> -->
+                <partDoughnut></partDoughnut>
+              </div>
             </el-col>
           </el-row>
         </el-card>
+        <br>
         <el-card shadow="hover">
+          <!-- <div slot="header" class="item-title" >
+            <span>检修统计</span>
+          </div> -->
+          <linechart></linechart>
         </el-card>
       </el-col>
 
       <el-col :span="6" :md="24" :lg="6">
         <el-card shadow="hover">
-          <div slot="header"
+          <!-- <div slot="header"
                class="item-title"
                size="small">代办任务
-          </div>
+          </div> -->
           <task></task>
         </el-card>
+        <br>
         <el-card shadow="hover">
+          <!-- <div slot="header" class="item-title" size="small">
+            <span>故障原因占比</span>
+          </div> -->
+          <charts></charts>
         </el-card>
       </el-col>
     </el-row>
@@ -86,11 +96,17 @@
 import centers from './models/centers.vue'
 import charts from './models/charts.vue'
 import task from './models/task.vue'
+import accountDoughnut from './models/accountDoughnut.vue'
+import partDoughnut from './models/partDoughnut.vue'
+import linechart from './models/linechart.vue'
 export default {
   components: {
     'centers': centers,
     'charts': charts,
-    'task': task
+    'task': task,
+    'accountDoughnut': accountDoughnut,
+    'partDoughnut': partDoughnut,
+    'linechart': linechart
   }
 }
 </script>
