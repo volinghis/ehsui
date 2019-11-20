4a46a8bf3de8644ed853d8ee973777f011e17ce6
Mapleaves <15229255171@163.com>
2019-11-20 09:46:28 +0800

    Update portal.vue
    修改首页布局样式

--- src/page/layout/content/portal.vue
+++ src/page/layout/content/portal.vue
@@ -1,96 +1,104 @@
 <template>
-  <div class="container" style="width: 100%" max-height="$store.state.contentHeight">
-    <el-row >
-      <el-col  :span="6" :md="24" :lg="6">
-        <el-card shadow="hover">
-          <template>
+  <div class="container">
+    <el-row :gutter="10" style="margin-bottom:10px;">
+      <el-col :span="6" :md="24" :lg="6" class="heightCol">
+        <div>
+          <el-card shadow='hover' v-bind:style="styleRowHeiheht" style="margin-left:5px;">
             <centers></centers>
-          </template>
-        </el-card>
+          </el-card>
+        </div>
       </el-col>
-
-      <el-col :span="12" :md="24" :lg="12">
-        <el-card shadow="hover">
-          <el-row>
-            <el-col :span="12">
-              <!-- <div>设备概况</div> -->
-              <accountDoughnut></accountDoughnut>
+      <el-col :span="18">
+        <div>
+          <el-row :gutter="10" style="margin-bottom:10px;">
+            <el-col :span="8">
+              <el-card shadow='hover' v-bind:style="styleColHeiheht">
+                <account-doughnut></account-doughnut>
+              </el-card>
             </el-col>
-            <el-col :span="12">
-              <div>
-                <!-- <div>备件概况</div> -->
-                <partDoughnut></partDoughnut>
-              </div>
+            <el-col :span="8">
+              <el-card shadow='hover' v-bind:style="styleColHeiheht">
+                <part-doughnut></part-doughnut>
+              </el-card>
+            </el-col>
+            <el-col :span="8">
+              <el-card shadow='hover' v-bind:style="styleColHeiheht">
+                <task></task>
+              </el-card>
             </el-col>
           </el-row>
-        </el-card>
-        <el-card shadow="hover">
-          <!-- <div slot="header" class="item-title" >
-            <span>检修统计</span>
-          </div> -->
-          <linechart></linechart>
-        </el-card>
-      </el-col>
-
-      <el-col :span="6" :md="24" :lg="6">
-        <el-card shadow="hover">
-          <!-- <div slot="header"
-               class="item-title"
-               size="small">代办任务
-          </div> -->
-          <task></task>
-        </el-card>
-        <el-card shadow="hover">
-          <!-- <div slot="header" class="item-title" size="small">
-            <span>故障原因占比</span>
-          </div> -->
-          <charts></charts>
-        </el-card>
+          <el-row :gutter="10">
+            <el-col :span="16">
+              <el-card shadow='hover' v-bind:style="styleColHeiheht">
+                <linechart></linechart>
+              </el-card>
+            </el-col>
+            <el-col :span="8">
+              <el-card shadow='hover' v-bind:style="styleColHeiheht">
+                <charts></charts>
+              </el-card>
+            </el-col>
+          </el-row>
+        </div>
       </el-col>
     </el-row>
-    <el-row>
-      <el-col :span="6" :md="24" :lg="6">
-        <el-card shadow="hover">
-          <template>
+    <el-row :gutter="10">
+      <el-col :span="6" class="elheight">
+        <div >
+          <el-card shadow='hover' v-bind:style="styleRowHeiheht" style="margin-left:5px;">
             <div slot="header" class="item-title" size="small" style="text-align:center;border-radius: 30px;">
-                 <el-button size="mini" round type="primary" style="weight:100px;">
-                    <span class="mainTitle">
-                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;贡&nbsp;献&nbsp;度&nbsp;季&nbsp;度&nbsp;排&nbsp;名&nbsp;&nbsp;&nbsp;&nbsp;
-                    </span>
-                  </el-button>
-              </div>
+              <el-button size="mini" round type="primary" >
+                <span style="weight:100px;font-size: large; font-weight: bold;">
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;贡&nbsp;献&nbsp;度&nbsp;季&nbsp;度&nbsp;排&nbsp;名&nbsp;&nbsp;&nbsp;&nbsp;
+                </span>
+              </el-button>
+            </div>
             <ranking></ranking>
-          </template>
-        </el-card>
+          </el-card>
+        </div>
       </el-col>
-      <el-col :span="12" :md="24" :lg="12">
-        <el-card shadow="hover">
-          <div slot="header" class="item-title" size="small">
-            <span class="mainTitle">新增检修记录</span>
-          </div>
-          <maintenanceRecord></maintenanceRecord>
-        </el-card>
-        <el-card shadow="hover">
-          <div slot="header" class="item-title" size="small">
-            <span class="mainTitle">动&nbsp;&nbsp;&nbsp;&nbsp;态</span>
-          </div>
-          <dynamic></dynamic>
-        </el-card>
-      </el-col>
-      <el-col :span="6" :md="24" :lg="6">
-        <el-card shadow="hover">
-          <div slot="header" class="item-title" size="small">
-            <span class="mainTitle">资料完整度</span>
-          </div>
-          <devices></devices>
-        </el-card>
-        <el-card shadow="hover">
-          <div slot="header" class="item-title" size="small">
-            <span class="mainTitle">热门关注</span>
-            <a href="#"><span style="float:right;color:#409EFF">更多</span></a>
-          </div>
-          <documents></documents>
-        </el-card>
+      <el-col :span="18">
+        <div>
+          <el-row :gutter="10" style="margin-bottom:10px;">
+            <el-col :span="16">
+              <el-card shadow='hover' v-bind:style="styleColHeiheht">
+                <div slot="header" class="item-title" size="small">
+                  <span class="mainTitle">新增检修记录</span>
+                </div>
+                <maintenance-record></maintenance-record>
+              </el-card>
+            </el-col>
+            <el-col :span="8">
+              <el-card shadow='hover' v-bind:style="styleColHeiheht">
+                <div slot="header" class="item-title" size="small">
+                  <span class="mainTitle">资料完整度</span>
+                </div>
+                <devices></devices>
+              </el-card>
+            </el-col>
+          </el-row>
+          <el-row :gutter="10">
+            <el-col :span="16">
+              <el-card shadow='hover' v-bind:style="styleColHeiheht">
+                <div slot="header" class="item-title" size="small">
+                  <span class="mainTitle">动&nbsp;&nbsp;&nbsp;&nbsp;态</span>
+                </div>
+                <dynamic></dynamic>
+              </el-card>
+            </el-col>
+            <el-col :span="8">
+              <el-card shadow='hover' v-bind:style="styleColHeiheht">
+                <div slot="header" class="item-title" size="small">
+                  <span class="mainTitle">热门关注</span>
+                  <a href="#"><span style="float:right;color:#409EFF">更多</span></a>
+                </div>
+                <div>
+                  <documents></documents>
+                </div>
+              </el-card>
+            </el-col>
+          </el-row>
+        </div>
       </el-col>
     </el-row>
   </div>
@@ -108,6 +116,17 @@ import dynamic from './models/dynamic.vue'
 import ranking from './models/ranking.vue'
 import documents from './models/documents.vue'
 export default {
+  data () {
+    return {
+      styleRowHeiheht: {
+        // height: this.nHeight
+        height: ''
+      },
+      styleColHeiheht: {
+        height: ''
+      }
+    }
+  },
   components: {
     'centers': centers,
     'charts': charts,
@@ -120,19 +139,24 @@ export default {
     'dynamic': dynamic,
     'documents': documents,
     'ranking': ranking
+  },
+  mounted: function () {
+    var h = document.querySelector('.heightCol').offsetWidth
+    this.styleRowHeiheht.height = h * 2 + 10 + 'px'
+    this.styleColHeiheht.height = h + 'px'
   }
 }
 </script>
 <style scoped>
-  .el-card__header{
-    padding:8px;
-  }
   .mainTitle{
     font-size: large;
     font-weight: bold;
     color: #646669
   }
-  .el-card {
-    border: 1px solid #ffffff;
+</style>
+<style>
+  .el-card__header {
+    padding: 10px 20px !important;
+    height: 45px !important;
   }
 </style>
