18eb3c8fb68dc495c159b3596b791d44b2a2fbb4
qjj <584419234@qq.com>
2019-12-11 14:07:32 +0800

    配置代理

--- vue.config.js
+++ vue.config.js
@@ -16,18 +16,18 @@ module.exports = {
       }
     }
   },
-  // devServer: {
-  //   proxy: {
-  //     '/ehs': {
-  //       target: 'http://127.0.0.1:18087/ehs',
-  //       changeOrigin: true,
-  //       pathRewrite: {
-  //         '^/ehs': '' // rewrite path
-  //       }
-  //     }
+  devServer: {
+    proxy: {
+      '/ehs': {
+        target: 'http://127.0.0.1:18087/ehs',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/ehs': '' // rewrite path
+        }
+      }
 
-  //   }
-  // },
+    }
+  },
   configureWebpack: config => {
     if (process.env.NODE_ENV !== 'production') {
       config.entry.app = ['./src/mocks/index.js', './src/main.js']
