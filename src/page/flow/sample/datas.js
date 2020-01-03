202240deb574d162f65c7c39773afc85a8ae8347
selfvolinghis <2629106344@qq.com>
2020-01-03 18:58:34 +0800

    提交流程页面

---
+++ src/page/flow/sample/datas.js
@@ -0,0 +1,14 @@
+export default {
+  data () {
+    return {
+      flowSample: {
+        content: ''
+      }
+    }
+  },
+  methods: {
+    handerSubmit: function (process) {
+      console.log(process)
+    }
+  }
+}
--- src/page/flow/sample/index.vue
+++ src/page/flow/sample/index.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-    开启流程页面
+    开启流程页面 param1:flow+routername,param2:流程对象
     this.GlobalMethods.openWin('flowroleedit', { processDefineKey: 'TestFlow' })
+   <el-input
+        v-model="flowSample.content"
+        :rows="4"
+        placeholder="内容"
+        />
   </div>
-</template>>
+</template>
 <script>
-export default {
-  methods: {
-    handerSubmit: function (process) {
-      console.log(process)
-    }
-  }
-}
+import datas from './datas.js'
+export default datas
 </script>
